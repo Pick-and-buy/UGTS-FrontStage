19b60e2b5f5c71c1a61e892893ba99de35a76697
Trung Nam <trungnam.work@gmail.com>
2024-06-25 22:30:09 +0700

    feat/update:search and search history

--- app/screens/Search.jsx
+++ app/screens/Search.jsx
@@ -1,41 +1,46 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useCallback, useEffect } from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   Text,
   View,
   TextInput,
   ActivityIndicator,
   FlatList,
-} from "react-native";
-import { useRoute, useFocusEffect } from '@react-navigation/native';
+  TouchableOpacity,
+  RefreshControl,
+} from 'react-native';
+import { useRoute, useNavigation } from '@react-navigation/native';
 import { FontAwesome } from '@expo/vector-icons';
-import styles from "../screens/css/search.style";
-import { searchPostsByTitle } from "../api/post";
-import { COLORS } from "../constants/theme";
-import Post from "./post/Post";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import styles from '../screens/css/search.style';
+import { searchPostsByTitle } from '../api/post';
+import { COLORS } from '../constants/theme';
+import Post from './post/Post';
 
 const Search = () => {
   const route = useRoute();
+  const navigation = useNavigation();
   const initialQuery = route.params?.query ?? '';
   const [searchQuery, setSearchQuery] = useState(initialQuery);
   const [loading, setLoading] = useState(false);
   const [results, setResults] = useState([]);
-  // console.log(results);
-  useFocusEffect(
-    React.useCallback(() => {
-      if (initialQuery) {
-        setSearchQuery(initialQuery);
-        fetchResults(initialQuery);
-      }
-    }, [initialQuery])
-  );
+  const [searchHistory, setSearchHistory] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+
+  useEffect(() => {
+    if (initialQuery) {
+      setSearchQuery(initialQuery);
+      fetchResults(initialQuery);
+    }
+    loadSearchHistory();
+  }, [initialQuery]);
 
   const fetchResults = async (query) => {
     setLoading(true);
     try {
       const response = await searchPostsByTitle(query);
       setResults(response.data.result);
+      await saveSearchHistory(query);
     } catch (error) {
       console.error(error);
     } finally {
@@ -47,6 +52,84 @@ const Search = () => {
     if (searchQuery.trim() !== '') {
       fetchResults(searchQuery);
     }
+  };
+
+  const saveSearchHistory = async (query) => {
+    try {
+      const currentHistory = await AsyncStorage.getItem('searchHistory');
+      let historyArray = currentHistory ? JSON.parse(currentHistory) : [];
+      if (!historyArray.includes(query)) {
+        historyArray.push(query);
+        await AsyncStorage.setItem('searchHistory', JSON.stringify(historyArray));
+        setSearchHistory(historyArray);
+      }
+    } catch (error) {
+      console.error('Failed to save search history:', error);
+    }
+  };
+
+  const loadSearchHistory = async () => {
+    try {
+      const history = await AsyncStorage.getItem('searchHistory');
+      if (history) {
+        setSearchHistory(JSON.parse(history));
+      }
+    } catch (error) {
+      console.error('Failed to load search history:', error);
+    }
+  };
+
+  const clearSearchHistory = async () => {
+    try {
+      await AsyncStorage.removeItem('searchHistory');
+      setSearchHistory([]);
+    } catch (error) {
+      console.error('Failed to clear search history:', error);
+    }
+  };
+
+  const removeSearchHistoryItem = async (item) => {
+    try {
+      const currentHistory = await AsyncStorage.getItem('searchHistory');
+      let historyArray = currentHistory ? JSON.parse(currentHistory) : [];
+      const updatedHistory = historyArray.filter((query) => query !== item);
+      await AsyncStorage.setItem('searchHistory', JSON.stringify(updatedHistory));
+      setSearchHistory(updatedHistory);
+    } catch (error) {
+      console.error('Failed to remove search history item:', error);
+    }
+  };
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    if (searchQuery.trim() !== '') {
+      fetchResults(searchQuery).finally(() => setRefreshing(false));
+    } else {
+      setRefreshing(false);
+    }
+  }, [searchQuery]);
+
+  const renderSearchHistoryItem = ({ item }) => (
+    <View style={styles.historyItemContainer}>
+      <TouchableOpacity onPress={() => handleSearchFromHistory(item)} style={styles.historyItem}>
+        <FontAwesome name="history" size={16} color="#AFAFAE" style={styles.historyIcon} />
+        <Text style={styles.historyText}>{item}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => removeSearchHistoryItem(item)} style={styles.removeButton}>
+        <FontAwesome name="times" size={16} color="#AFAFAE" />
+      </TouchableOpacity>
+    </View>
+  );
+
+  const handleSearchFromHistory = (query) => {
+    setSearchQuery(query);
+    fetchResults(query);
+  };
+
+  const clearSearchInput = () => {
+    setSearchQuery('');
+    setResults([]); // Clear the search results
+    loadSearchHistory(); // Load search history to display
   };
 
   return (
@@ -68,25 +151,53 @@ const Search = () => {
               placeholderTextColor="#AFAFAE"
               style={styles.textInput}
             />
+            {searchQuery.trim() !== '' && (
+              <TouchableOpacity onPress={clearSearchInput} style={styles.clearInputButton}>
+                <FontAwesome name="times-circle" size={16} color="#AFAFAE" />
+              </TouchableOpacity>
+            )}
           </View>
         </View>
+        {!loading && results.length === 0 && (
+          <View style={styles.notificationContainer}>
+            <Text style={styles.notificationText}>Sản phẩm bạn tìm kiếm không tồn tại</Text>
+          </View>
+        )}
+        {results.length === 0 && searchHistory.length > 0 && (
+          <View style={styles.historyContainer}>
+            <Text style={styles.historyTitle}>Lịch sử tìm kiếm</Text>
+            <FlatList
+              data={searchHistory}
+              renderItem={renderSearchHistoryItem}
+              keyExtractor={(item, index) => index.toString()}
+              refreshControl={
+                <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+              }
+            />
+            <TouchableOpacity onPress={clearSearchHistory} style={styles.clearButton}>
+              <Text style={styles.clearButtonText}>Xóa lịch sử</Text>
+            </TouchableOpacity>
+          </View>
+        )}
         <View style={styles.content}>
           {loading ? (
             <ActivityIndicator size="large" color={COLORS.primary} />
           ) : (
-            <View style={styles.row}>
-              {
-                results.map(post => (
-                  <Post key={post.id} post={post} />
-                ))
+            <FlatList
+              data={results}
+              renderItem={({ item }) => <Post key={item.id} post={item} />}
+              keyExtractor={(item) => item.id.toString()}
+              numColumns={3}
+              refreshControl={
+                <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
               }
-            </View>
-
+              columnWrapperStyle={styles.row}
+            />
           )}
         </View>
       </View>
     </SafeAreaView>
   );
-};
+}
 
 export default Search;
--- app/screens/css/search.style.js
+++ app/screens/css/search.style.js
@@ -4,8 +4,6 @@ import { COLORS } from "../../constants/theme";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: COLORS.white,
-    // padding: 16,
     width: "100%",
   },
   header: {
@@ -26,11 +24,14 @@ const styles = StyleSheet.create({
     padding: 6,
     flex: 1,
     marginBottom: 10,
-    marginHorizontal: 10
+    marginHorizontal: 10,
   },
   textInput: {
     flex: 1,
     marginLeft: 8,
+  },
+  clearInputButton: {
+    padding: 8,
   },
   content: {
     width: '100%',
@@ -47,22 +48,77 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    marginTop: 20, // Adjust this margin as per your design
   },
   emptyText: {
     fontSize: 18,
     color: "#999",
   },
   row: {
-    width: "98%",
-    justifyContent: "space-around",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    alignItems: "center",
+    width: "96%",
+    justifyContent: "flex-start",
     gap: 5,
     marginHorizontal: "auto",
-    marginTop: "-4%",
-  }
-
+    marginBottom: 5,
+  },
+  historyContainer: {
+    paddingHorizontal: 16,
+    paddingTop: 16,
+  },
+  historyTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  historyItemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingTop: 10,
+    paddingBottom: 6,
+    borderBottomWidth: 1,
+    borderBottomColor: COLORS.gray2,
+  },
+  historyIcon: {
+    marginRight: 12,
+  },
+  historyItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  historyText: {
+    fontSize: 16,
+    color: COLORS.gray,
+  },
+  removeButton: {
+    paddingHorizontal: 12,
+  },
+  noHistoryText: {
+    color: COLORS.gray,
+    textAlign: 'center',
+    paddingVertical: 16,
+  },
+  clearButton: {
+    marginTop: 16,
+    paddingVertical: 12,
+    alignItems: 'center',
+    backgroundColor: COLORS.primary,
+    borderRadius: 8,
+  },
+  clearButtonText: {
+    color: COLORS.white,
+    fontWeight: 'bold',
+  },
+  notificationContainer: {
+    backgroundColor: COLORS.lightGray,
+    paddingVertical: 8,
+  },
+  notificationText: {
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: COLORS.red,
+  },
 });
 
 export default styles;
