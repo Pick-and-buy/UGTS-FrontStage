22a5b99b7d77490d9220ab2e7c152d50ecbf0f66
Trung Nam <trungnam.work@gmail.com>
2024-07-13 21:29:56 +0700

    update(ui):add label sold for post

--- app/screens/post/Post.jsx
+++ app/screens/post/Post.jsx
@@ -3,7 +3,7 @@ import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
 import { GestureHandlerRootView, TouchableOpacity } from 'react-native-gesture-handler';
 import styles from '../css/post.style';
 import { useNavigation } from '@react-navigation/native';
-
+import Svg, { Polygon } from 'react-native-svg';
 const image = { uri: "https://docs.expo.dev/static/images/tutorial/background-image.png" };
 
 const Post = ({ post, type }) => {
@@ -28,6 +28,9 @@ const Post = ({ post, type }) => {
                     onPress={() => navigation.navigate('post-details', { postId: post.id, type })}
                 >
                     <ImageBackground source={{ uri: post?.product?.images[0]?.imageUrl }} style={styles.image}>
+                        <View style={styles.label} >
+                            <Text style={styles.labelText}>Đã bán</Text>
+                        </View>
                         <Text style={styles.text}>
                             <Text style={styles.currency}>đ</Text>
                             {formattedPrice}
