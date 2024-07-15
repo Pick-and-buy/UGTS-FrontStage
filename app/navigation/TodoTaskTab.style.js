cf71297badd628e84bc3ca2e24e719eb281dfb17
Trung Nam <trungnam.work@gmail.com>
2024-07-15 13:35:55 +0700

    feat(ui): todo task list item for buyer

---
+++ app/navigation/TodoTaskTab.style.js
@@ -0,0 +1,29 @@
+import { Dimensions, StyleSheet } from "react-native";
+import { COLORS, SIZES } from "../constants/theme";
+
+
+const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+    },
+    tabBarStyle: {
+        width: "100%",
+        elevation: 0, // This will remove the shadow on Android
+        shadowOpacity: 0, // This will remove the shadow on iOS
+    },
+    tabActive: {
+        fontSize: 14,
+        color: COLORS.primary,
+    },
+    tab: {
+        fontSize: 14,
+        color: "#AFAFAE",
+    },
+    tabBarIndicator:{
+        backgroundColor: COLORS.primary, // This will be the color of the underline
+        width: 10,
+    }
+
+})
+
+export default styles
---
+++ app/screens/css/buyer.style.js
@@ -0,0 +1,59 @@
+import { Dimensions, StyleSheet } from "react-native";
+import { COLORS, SHADOWS } from "../../constants/theme";
+
+
+const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        marginBottom: 20
+    },
+    card: {
+        width: "94%",
+        height: 100,
+        flexDirection: 'row',
+        marginHorizontal: "auto",
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        marginVertical: 4,
+        padding: 10,
+        borderRadius: 20,
+        ...SHADOWS.small
+    },
+    image: {
+        width: "30%",
+        height: "100%",
+        borderRadius: 15,
+        marginRight: 10,
+        objectFit: "cover"
+    },
+    info: {
+        flex: 1,
+    },
+    itemTitle: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    shop: {
+        fontSize: 14,
+        color: '#8F8F8F',
+    },
+    price: {
+        fontSize: 14,
+        color: '#4CAF50',
+    },
+    statusButton: {
+        marginLeft: 8,
+        paddingHorizontal: 25,
+        paddingVertical: 8,
+        borderRadius: 20,
+        backgroundColor: COLORS.primary,
+    },
+    statusText: {
+        color: '#fff',
+        fontSize: 12,
+        fontWeight: '400',
+    },
+
+})
+
+export default styles
