92e402a3c20ea74a8001a678abef01b44b78b7d1
Trung Nam <trungnam.work@gmail.com>
2024-07-24 21:08:45 +0700

    update:refresh status in header when mark notification read

--- app/context/NotificationContext.js
+++ app/context/NotificationContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { getNotificationsByUserId } from '../api/user';
+import { getNotificationsByUserId, updateNotificationsReadStatus } from '../api/user';
 import { useAuth } from './AuthContext';
 
 const NotificationContext = createContext();
@@ -11,16 +11,24 @@ export const useNotifications = () => {
 export const NotificationProvider = ({ children }) => {
     const { user } = useAuth();
     const [notifications, setNotifications] = useState([]);
-    console.log("user in noti context", user);
+
     const fetchNotifications = async () => {
         try {
             if (user && user.id) {
                 const data = await getNotificationsByUserId(user.id);
                 setNotifications(data.result);
-                // console.log("Notifications fetched:", data);
             }
         } catch (error) {
             console.error('Fetching notifications failed:', error);
+        }
+    };
+
+    const markNotificationAsRead = async (notificationId) => {
+        try {
+            await updateNotificationsReadStatus(notificationId);
+            fetchNotifications();
+        } catch (error) {
+            console.error('Updating notification read status failed:', error);
         }
     };
 
@@ -31,7 +39,7 @@ export const NotificationProvider = ({ children }) => {
     }, [user]);
 
     return (
-        <NotificationContext.Provider value={{ notifications }}>
+        <NotificationContext.Provider value={{ notifications, markNotificationAsRead }}>
             {children}
         </NotificationContext.Provider>
     );
--- app/screens/notification/Notification.jsx
+++ app/screens/notification/Notification.jsx
@@ -2,46 +2,20 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, SafeAreaView, TouchableOpacity, Image, FlatList } from 'react-native';
 import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
 import styles from '../css/notification.style';
-
-import { getNotificationsByUserId, updateNotificationsReadStatus } from '../../api/user';
 import { useAuth } from '../../context/AuthContext';
+import { useNotifications } from '../../context/NotificationContext';
 const profile = "https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg";
 
 const Notification = ({ navigation }) => {
     const { user } = useAuth();
-    const [notifications, setNotifications] = useState([]);
-    console.log("user id in noti", user?.id);
-    // console.log(">>>notification ", notifications);
-
-    useEffect(() => {
-        if (user?.id) {
-            fetchNotifications();
-        }
-    }, [user]);
-
-    const fetchNotifications = async () => {
-        try {
-            const notificationsData = await getNotificationsByUserId(user?.id);
-            setNotifications(notificationsData.result);
-        } catch (error) {
-            console.error('Error fetching notifications:', error);
-        }
-    };
+    const { notifications, markNotificationAsRead } = useNotifications();
 
     const handleNotificationRead = async (notificationId) => {
-        console.log(notificationId);
-        try {
-            const rs = await updateNotificationsReadStatus(notificationId);
-            console.log(rs);
-            fetchNotifications();
-        } catch (error) {
-            console.error('Error fetching notifications read:', error);
-        }
-    }
+        await markNotificationAsRead(notificationId);
+    };
 
     const renderNotificationItem = ({ item }) => {
         const notificationTextStyle = item.read ? styles.readNotificationText : styles.unreadNotificationText;
-        // console.log(item.read);
         return (
             <TouchableOpacity style={styles.notificationItem} onPress={() => handleNotificationRead(item.notificationId)}>
                 <Image
