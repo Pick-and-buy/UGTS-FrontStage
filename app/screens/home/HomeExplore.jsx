ecd2c47926df6574578583b7af5513384e8a51f7
Trung Nam <trungnam.work@gmail.com>
2024-08-17 01:57:51 +0700

    fix(ui):filter scroll when lack of view in small device

--- app/screens/home/HomeExplore.jsx
+++ app/screens/home/HomeExplore.jsx
@@ -7,6 +7,7 @@ import {
     ActivityIndicator,
     RefreshControl,
     TouchableOpacity,
+    FlatList,
 } from "react-native";
 import { AntDesign } from '@expo/vector-icons';
 import { COLORS, SHADOWS } from "../../constants/theme";
@@ -33,7 +34,7 @@ const HomeExplore = ({ navigation }) => {
 
             // Sort posts to have boosted posts at the top
             posts = posts.sort((a, b) => b.boosted - a.boosted);
-            
+
             setPosts(posts);
             setFilteredPosts(posts);
         } catch (error) {
@@ -66,6 +67,29 @@ const HomeExplore = ({ navigation }) => {
         }
     };
 
+    const renderFilterItem = ({ item }) => (
+        <TouchableOpacity
+            style={[styles.filterButton, filter === item.value && styles.activeFilter]}
+            onPress={() => filterPosts(item.value)}
+        >
+            <Text
+                style={[
+                    styles.filterButtonText,
+                    filter === item.value ? styles.activeFilterText : styles.inactiveFilterText,
+                ]}
+            >
+                {item.label}
+            </Text>
+        </TouchableOpacity>
+    );
+
+    const filterOptions = [
+        { label: 'Tất cả', value: 'All' },
+        { label: 'Nổi bật', value: 'Hot' },
+        { label: 'Chưa bán', value: true },
+        { label: 'Đã bán', value: false },
+    ];
+
     return (
         <ScrollView
             style={styles.container}
@@ -80,30 +104,19 @@ const HomeExplore = ({ navigation }) => {
                 <Text style={styles.heading}>Bài đăng</Text>
                 <View style={styles.filterContainer}>
                     <AntDesign name="filter" size={26} color="black" />
-                    <TouchableOpacity
-                        style={[styles.filterButton, filter === 'All' && styles.activeFilter]}
-                        onPress={() => filterPosts('All')}
-                    >
-                        <Text style={[styles.filterButtonText, filter === 'All' ? styles.activeFilterText : styles.inactiveFilterText]}>Tất cả</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={[styles.filterButton, filter === 'Hot' && styles.activeFilter]}
-                        onPress={() => filterPosts('Hot')}
-                    >
-                        <Text style={[styles.filterButtonText, filter === 'Hot' ? styles.activeFilterText : styles.inactiveFilterText]}>Nổi bật</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={[styles.filterButton, filter === true && styles.activeFilter]}
-                        onPress={() => filterPosts(true)}
-                    >
-                        <Text style={[styles.filterButtonText, filter === true ? styles.activeFilterText : styles.inactiveFilterText]}>Chưa bán</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={[styles.filterButton, filter === false && styles.activeFilter]}
-                        onPress={() => filterPosts(false)}
-                    >
-                        <Text style={[styles.filterButtonText, filter === false ? styles.activeFilterText : styles.inactiveFilterText]}>Đã bán</Text>
-                    </TouchableOpacity>
+                    <FlatList
+                        horizontal
+                        data={filterOptions}
+                        renderItem={renderFilterItem}
+                        keyExtractor={(item) => item.value.toString()}
+                        showsHorizontalScrollIndicator={false}
+                        contentContainerStyle={{
+                            paddingHorizontal: 10, justifyContent: "center",
+                            alignItems: "center",
+                            gap: 10,
+                            height:50
+                        }}
+                    />
                 </View>
                 {loading ? (
                     <ActivityIndicator size="large" color={COLORS.primary} />
