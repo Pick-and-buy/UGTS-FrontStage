921968dfe286a3a7212aeab51fe93252ce516180
Trung Nam <trungnam.work@gmail.com>
2024-08-21 23:10:36 +0700

    update(icon): my logo

--- app/api/axiosInstance.js
+++ app/api/axiosInstance.js
@@ -5,7 +5,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const axiosInstance = axios.create({
 
 
-    baseURL: 'http://192.168.1.6:8080/api/v1/', // replace with your API base URL
+    baseURL: 'http://192.168.1.7:8080/api/v1/', // replace with your API base URL
 
 
     // headers: {
